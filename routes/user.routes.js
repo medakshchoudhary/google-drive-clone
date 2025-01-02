7fd0e04d9b4022100e2b6c342e63762430c29c7b
Daksh Choudhary <ch.daksh01@gmail.com>
2025-01-03 01:05:29 +0530

    added tailwind and flowbite

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,8 @@ const express = require("express")
 const router = express.Router()
 
 
-router.get("/test",(req,res)=>{
-    res.render("index")
+router.get("/register",(req,res)=>{
+    res.render("register")
 })
 
 module.exports = router;
